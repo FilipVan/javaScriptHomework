5edd907875ba2c376305857f469cf94b617d37f4
FilipVan <vancevski91@gmail.com>
2016-11-18 18:09:14 +0100

    Update WizardFilipUpdated.js

--- javaScriptHomework/WizardFilipUpdated.js
+++ javaScriptHomework/WizardFilipUpdated.js
@@ -1,4 +1,4 @@
-var currentExp = parseInt(prompt("Enter Current XP"));
+/*var currentExp = parseInt(prompt("Enter Current XP"));
 //var neededExp = 100-currentExp%100+currentExp; ova i red 3 se ako sakas samo da vnesuvas currentExp a neededExp i winExp da si gi imaat odnapred utvrdenite vrednosti
 //var winExp = 75;
 var neededExp = parseInt(prompt("Enter Needed XP"));
@@ -20,3 +20,22 @@ function willLevelUp(currentExp,neededExp,winExp){
 
 
 alert(willLevelUp(currentExp,neededExp,winExp));
+*/
+//ova podole e od cas update
+
+function willLevelUp(currentExp, neededExp, winExp){
+    return currentExp + winExp >= neededExp;
+}
+
+function alertUser(shouldAlert, message){
+    if(shouldAlert){
+        alert(message);
+        return true;
+    } else return false;
+}
+
+
+if(!alertUser(willLevelUp(125, 160, 40), "You have leveled up!"));{
+   (alertUser(!willLevelUp(125, 260, 40), "You have not leveled up!");
+
+
